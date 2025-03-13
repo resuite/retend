6fd42fbac5f57302d6ec64e5dd57d544fef69bda
adebola-io <adebolaakomolafe@gmail.com>
2025-03-13 21:38:32 +0100

    chore: remove unused packages and configuration files

--- packages/unfinished/source/jsx-runtime/types.ts
+++
@@ -1,1 +0,0 @@
-export type { JSX } from './index.d.ts';
--- packages/unfinished/source/library/index.js
+++
@@ -1,7 +0,0 @@
-export * from './jsx.js';
-export * from './for.js';
-export * from './if.js';
-export * from './switch.js';
-export * from './observer.js';
-export * from './context.js';
-export * from './consistent.js';
