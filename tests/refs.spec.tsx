860cad6caff94dc7a176539f4c9cb89c9876d78e
adebola-io <adebolaakomolafe@gmail.com>
2025-03-14 09:15:16 +0100

    feat(tests): Add tests for refs

---
+++ tests/refs.spec.tsx
@@ -0,0 +1,46 @@
+import { describe, it, expect, vi } from 'vitest';
+import { Cell } from 'retend';
+import { getGlobalContext } from 'retend/context';
+import { browserSetup, vDomSetup } from './setup.ts';
+import type { VNode } from 'retend/v-dom';
+
+const runTests = () => {
+  it('should assign a ref to an element', () => {
+    const elementRef = Cell.source<HTMLElement | null>(null);
+    const element = <div ref={elementRef}>Hello, world!</div>;
+
+    expect(elementRef.value).toEqual(element);
+  });
+
+  it('should work with functional components', () => {
+    const { window } = getGlobalContext();
+    const elementRef = Cell.source<HTMLElement | null>(null);
+    const callback = vi.fn();
+
+    const MyComponent = () => {
+      return <div ref={elementRef}>Hello, world!</div>;
+    };
+
+    const element = (<MyComponent />) as unknown as HTMLElement & VNode;
+
+    expect(elementRef.value).toEqual(element);
+    expect(elementRef.value).toBeInstanceOf(window.HTMLElement);
+
+    elementRef.listen(callback);
+    const newInstance = <MyComponent />;
+    expect(callback).toHaveBeenCalledTimes(1);
+    expect(elementRef.value).toEqual(newInstance);
+  });
+};
+
+describe('Refs', () => {
+  describe('Browser', () => {
+    browserSetup();
+    runTests();
+  });
+
+  describe('VDom', () => {
+    vDomSetup();
+    runTests();
+  });
+});
