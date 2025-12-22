c94437cede3615e81760d67f437f30cab497e3b3
Sefunmi <adebolaakomolafe@gmail.com>
2025-12-22 08:45:01 +0100

    Remove example applications from workspaces
    Delete hmr-test, unique-test, and utils-testing. Update the workspace
    configuration and ignore the examples directory.

--- examples/hmr-test/source/views/teleported.tsx
+++
@@ -1,9 +0,0 @@
-import { Teleport } from 'retend-web';
-
-export function TeleportedElement() {
-  return (
-    <div>
-      <Teleport to="body"> This element should appear in the body</Teleport>
-    </div>
-  );
-}
--- examples/utils-testing/source/views/test-pages/page2.tsx
+++
@@ -1,6 +0,0 @@
-import styles from './page2.module.css';
-import './shared.css';
-
-export default function Page2() {
-  return <div class={styles.page2}>Page 2</div>;
-}
