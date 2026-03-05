123a938ae02499c61a8b7a0f351ebecbef920366
Sefunmi <adebolaakomolafe@gmail.com>
2026-03-05 22:09:30 +0100

    Encapsulate devtools panel in Shadow DOM

--- packages/retend-web-devtools/source/components/Panel.tsx
+++ packages/retend-web-devtools/source/components/Panel.tsx
@@ -1,5 +1,5 @@
-import { If } from 'retend';
-import { Teleport } from 'retend-web';
+import { Cell, If, onConnected } from 'retend';
+import { ShadowRoot, Teleport } from 'retend-web';
 
 import { useDevToolsRenderer } from '../core/DevToolsRendererScope';
 import { usePanelState } from '../hooks/usePanelState';
@@ -9,56 +9,73 @@ import { HighlightOverlay } from './HighlightOverlay';
 import { InspectorPanel } from './InspectorPanel';
 import { PanelHeaderTools } from './PanelHeaderTools';
 
+const VITE_STYLE_KEY = '[data-vite-dev-id*="retend-web-devtools"]';
+
 export function Panel() {
   const devRenderer = useDevToolsRenderer();
   const panel = usePanelState();
+  const divRef = Cell.source<HTMLElement | null>(null);
+
+  onConnected(divRef, (div) => {
+    requestAnimationFrame(() => {
+      const styleTags = document.querySelectorAll(VITE_STYLE_KEY);
+      for (const styleTag of styleTags) div.shadowRoot?.prepend(styleTag);
+    });
+  });
 
   return (
     <Teleport to="body">
-      <div
-        class={[
-          classes.panel,
-          {
-            [classes.positionBottomRight]: panel.isBottomRight,
-            [classes.positionBottomLeft]: panel.isBottomLeft,
-            [classes.positionTopRight]: panel.isTopRight,
-            [classes.positionTopLeft]: panel.isTopLeft,
-          },
-        ]}
-        data-retend-devtools=""
-      >
-        {If(panel.panelIsOpen, () => (
+      <div ref={divRef} style={{ display: 'contents' }}>
+        <ShadowRoot>
           <div
             class={[
-              classes.panelWorkspace,
+              classes.panel,
               {
-                [classes.inspectorLeft]: panel.isInspectorLeft,
-                [classes.inspectorRight]: panel.isInspectorRight,
+                [classes.positionBottomRight]: panel.isBottomRight,
+                [classes.positionBottomLeft]: panel.isBottomLeft,
+                [classes.positionTopRight]: panel.isTopRight,
+                [classes.positionTopLeft]: panel.isTopLeft,
               },
             ]}
+            data-retend-devtools=""
           >
-            <div class={classes.content}>
-              <div class={classes.header}>
-                <span class={classes.title}>Component Tree</span>
-                <PanelHeaderTools />
+            {If(panel.panelIsOpen, () => (
+              <div
+                class={[
+                  classes.panelWorkspace,
+                  {
+                    [classes.inspectorLeft]: panel.isInspectorLeft,
+                    [classes.inspectorRight]: panel.isInspectorRight,
+                  },
+                ]}
+              >
+                <div class={classes.content}>
+                  <div class={classes.header}>
+                    <span class={classes.title}>Component Tree</span>
+                    <PanelHeaderTools />
+                  </div>
+                  <div class={classes.body}>
+                    <ComponentTree />
+                  </div>
+                </div>
+                {If(devRenderer.selectedNode, (selectedNode) => (
+                  <InspectorPanel selectedNode={selectedNode} />
+                ))}
               </div>
-              <div class={classes.body}>
-                <ComponentTree />
-              </div>
-            </div>
-            {If(devRenderer.selectedNode, (selectedNode) => (
-              <InspectorPanel selectedNode={selectedNode} />
             ))}
+            <HighlightOverlay />
+            <button
+              type="button"
+              class={[
+                classes.button,
+                { [classes.buttonOpen]: panel.panelIsOpen },
+              ]}
+              onClick={panel.togglePanel}
+            >
+              RT
+            </button>
           </div>
-        ))}
-        <HighlightOverlay />
-        <button
-          type="button"
-          class={[classes.button, { [classes.buttonOpen]: panel.panelIsOpen }]}
-          onClick={panel.togglePanel}
-        >
-          RT
-        </button>
+        </ShadowRoot>
       </div>
     </Teleport>
   );
